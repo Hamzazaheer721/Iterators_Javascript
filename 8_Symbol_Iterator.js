7c54b7785d4d6fd047c60a0d715933687e58aa46
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-12 21:42:06 +0500

    Implementing the iterator

---
+++ 8_Symbol_Iterator.js
@@ -0,0 +1,27 @@
+// usage of Generator functions
+// implementing iterables
+// When you implement an iterator, you have to make an iterable object manually which
+// return next() method. P.s you have to manually save the states.
+// it gets really hard sometimes. since generators are also iterables, they can be
+// used to implement iterables without extra boilerplate code.
+
+const iterableObj = {
+  [Symbol.iterator]() {
+    let step = 0
+    return {
+      next: () => {
+        step++
+        if (step === 1) return { value: "first", done: false }
+        else if (step === 2) return { value: "second", done: false }
+        else if (step === 3) return { value: "third", done: false }
+        return { value: "", done: true }
+      }
+    }
+  }
+}
+
+for (val of iterableObj) {
+  console.log(val)
+}
+
+//first, second and third
