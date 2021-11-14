a76b791a3282e898e5c9e1828124b8067b223de5
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-14 21:48:01 +0500

    !fix: added Symbol iterator

--- 8_ii_Symbol_iterator.js
+++ 8_ii_Symbol_iterator.js
@@ -1,2 +1,10 @@
 // Symbol iterator basically defines the default iterator for the object
 // it is bascially used by for .... of
+const iterable = {}
+iterable[Symbol.iterator] = function* () {
+  yield 1
+  yield 2
+  yield 3
+}
+
+console.log([...iterable])
